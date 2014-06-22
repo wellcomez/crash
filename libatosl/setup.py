42a1611f1b37812ab2a206ca4ce4bd23b075be5a
ll <zhu.jialai@gmail.com>
2014-06-22 21:36:18 +0800

    delete

--- libatosl/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-#/Users/zhujialai/Library/Developer/Xcode/DerivedData/libatosl-hidgoaqisgitjlcopwrelmrjiwzx/Build/Products/Release
-module1 = Extension('atosl', 
-                    sources = ['pyatosl.cpp',"common.cc","subprograms.cc","libatosl.cc","pySymboleFile.cpp","symbolFile.cpp"],
-                    include_dirs=['/usr/local/Cellar/libdwarf/20130729/include/'],
-                    libraries=['dwarf', 'bfd','iberty','opcodes'],
-                    library_dirs=["."]) 
-setup(name="PackageName atosl", version="1.0",description="This is Test",ext_modules=[module1])
