a905108ddb8f1f2e0e673f5f5524081eb0088c93
zhujialai <zhu.jialai@gmail.com>
2014-06-24 14:03:39 +0800

    更新解析

--- libatosl/r.sh
+++ libatosl/r.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-# test.sh
- 
-MAX=1000
- 
-for (( i = 0; i < MAX ; i ++ ))
-do
-	    echo $i
-		#python ./logparser.py ./config.ini.2.2.10460.667
-	    python ./p.py ./config.ini.2.2.10460.667
-	    python ./logparser.py ./config.ini.2.2.10460.667
-	    python ./file2db.py ./config.ini.2.2.10460.667
-		#python ./sql.py ./config.ini.2.2.10460.667
-	    echo "sleep......."
-	    sleep 1800s
-done
+U2FsdGVkX194ECcAAAAAAO7wR7ZeeuXDKKSClOjLvmM=
